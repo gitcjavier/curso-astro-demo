b01a77fed08194afa1f345a7bd840188f0a312da
Javier Castro <javiercastro.everis@latam.com>
2025-07-06 20:18:18 -0400

    Add Vercel configuration and fix build structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import vercel from '@astrojs/vercel';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'static',
+  output: 'server',
   adapter: vercel({
     webAnalytics: { enabled: true }
   }),
